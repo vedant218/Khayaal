97e4c1044979530c6834c36de2a3c713928f3079
vedant218 <vedantdeshmukh10@gmail.com>
2022-03-10 03:28:33 +0530

    Vedants edits

--- app/src/main/java/com/example/hackathonsplashscreen/CareFragment.java
+++ app/src/main/java/com/example/hackathonsplashscreen/CareFragment.java
@@ -3,18 +3,23 @@ package com.example.hackathonsplashscreen;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class CareFragment extends Fragment {
 
-
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_care, container, false);
     }
+
+
 }
