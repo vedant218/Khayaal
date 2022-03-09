b2fe628144e11085320561a3203e6c81e86dad3f
vedant218 <vedantdeshmukh10@gmail.com>
2022-03-10 02:39:43 +0530

    Vedants edits

--- app/src/main/java/com/example/hackathonsplashscreen/home.java
+++ app/src/main/java/com/example/hackathonsplashscreen/home.java
@@ -1,14 +1,45 @@
 package com.example.hackathonsplashscreen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class home extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment = new HomeFragment();
+    CareFragment careFragment = new CareFragment();
+    TimeFragment timeFragment = new TimeFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigationView= findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                        return true;
+                    case R.id.Caretaker:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,careFragment).commit();
+                        return true;
+                    case R.id.timeline:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,timeFragment).commit();
+                        return true;
+
+                }
+                return false;
+            }
+        });
     }
 }
