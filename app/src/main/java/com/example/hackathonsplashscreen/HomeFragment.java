ed149416af4927d87aba34c8ca47405f888f2fbd
vedant218 <vedantdeshmukh10@gmail.com>
2022-03-10 08:57:45 +0530

    Vedants edits

--- app/src/main/java/com/example/hackathonsplashscreen/HomeFragment.java
+++ app/src/main/java/com/example/hackathonsplashscreen/HomeFragment.java
@@ -12,6 +12,7 @@ import android.view.ViewGroup;
 public class HomeFragment extends Fragment {
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
--- app/src/main/java/com/example/hackathonsplashscreen/home.java
+++ app/src/main/java/com/example/hackathonsplashscreen/home.java
@@ -6,11 +6,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class home extends AppCompatActivity {
+
+    Button button1;
+    Button button2;
 
     BottomNavigationView bottomNavigationView;
     HomeFragment homeFragment = new HomeFragment();
@@ -21,6 +26,24 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        button1 = findViewById(R.id.bot);
+        button2 = findViewById(R.id.voice);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent patientIntent = new Intent(getApplicationContext(), patient.class);
+                startActivity(patientIntent);
+            }
+
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Audio.class);
+                startActivity(intent);
+            }
+
+        });
 
         bottomNavigationView= findViewById(R.id.bottom_navigation);
 
